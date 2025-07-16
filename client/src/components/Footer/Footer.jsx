5647d9eeaa269e24df1d980b6a279d8da7a12e85
musmankkh <42khan0@gmail.com>
2025-07-17 01:55:22 +0500

    create footer component file

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="bg-dark text-white py-3 mt-auto">
+    <div className="container text-center">
+      <small>&copy; 2025 Shortly. All rights reserved.</small>
+    </div>
+  </footer>
+);
+
+export default Footer;
